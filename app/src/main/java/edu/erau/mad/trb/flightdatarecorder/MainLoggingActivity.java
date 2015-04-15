c05d532bfb2767f72e64dc12c1b60f81e511abec
Thomas Bassa <ThomasBassa@users.noreply.github.com>
2015-04-14 22:39:55 -0400

    Failed chronometer experiments...

--- app/src/main/java/edu/erau/mad/trb/flightdatarecorder/MainLoggingActivity.java
+++ app/src/main/java/edu/erau/mad/trb/flightdatarecorder/MainLoggingActivity.java
@@ -1,19 +1,38 @@
 package edu.erau.mad.trb.flightdatarecorder;
 
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Chronometer;
 
 
-public class MainLoggingActivity extends ActionBarActivity {
+public class MainLoggingActivity extends ActionBarActivity implements
+        Chronometer.OnChronometerTickListener {
+
+    private Chronometer chronometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_logging);
+
+        chronometer = (Chronometer) findViewById(R.id.timer);
+        chronometer.setOnChronometerTickListener(this);
+
+        int count = 0;
+        for(long x = 0; x < (1000L * 60L * 60L * 30L); x += (1000L * 60L * 60L)) {
+            Log.d("ChronTest", String.format("hour %d, millis %d becomes:",
+                    count, x));
+            Log.d("ChronTest", DateFormat.format("HH:mm:ss", x).toString());
+            Log.d("ChronTest", DateFormat.format("kk:mm:ss", x).toString());
+            count++;
+        }
     }
 
 
@@ -39,8 +58,38 @@ public class MainLoggingActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        chronometer.stop();
+    }
+
     public void goToHistory(View view) {
         Intent startHist = new Intent(this, HistoryActivity.class);
         startActivity(startHist);
+    }
+
+    public void resetTimer(View view) {
+        chronometer.stop();
+        chronometer.setBase(SystemClock.elapsedRealtime() - (1000L * 60L * 60L));
+        onChronometerTick(chronometer);
+    }
+
+    public void startTimer(View view) {
+        //chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.start();
+    }
+
+    public void stopTimer(View view) {
+        chronometer.stop();
+    }
+
+    @Override
+    public void onChronometerTick(Chronometer chronometer) {
+        //Via...
+        // http://stackoverflow.com/questions/4897665/android-chronometer-format
+        long t = SystemClock.elapsedRealtime() - chronometer.getBase();
+        Log.v("Chron", "Delta t = " + t);
+        chronometer.setText(DateFormat.format("HH:mm:ss", t));
     }
 }
