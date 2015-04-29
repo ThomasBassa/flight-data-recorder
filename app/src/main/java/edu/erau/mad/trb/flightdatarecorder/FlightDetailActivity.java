b0d250e037ee171019bb391e5ad1850042edf8bf
Thomas Bassa <ThomasBassa@users.noreply.github.com>
2015-04-29 13:41:12 -0400

    Document FlightDetailActivity.

--- app/src/main/java/edu/erau/mad/trb/flightdatarecorder/FlightDetailActivity.java
+++ app/src/main/java/edu/erau/mad/trb/flightdatarecorder/FlightDetailActivity.java
@@ -1,4 +1,8 @@
 package edu.erau.mad.trb.flightdatarecorder;
+/* DevicePosAndOrient.java
+ * SE395A Final Project
+ * by Thomas Bassa
+ * A Java class to handle the display of flight details on a map. */
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -7,22 +11,27 @@ import android.widget.Toast;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
-//TODO Document FlightDetailActivity
+/** An activity that displays the points of a specified flight on a Google Map */
 public class FlightDetailActivity extends FragmentActivity {
 
     public static final String INTENT_ID_INFO = "edu.erau.mad.trb" +
             ".flightdatarecorder.INTENT_FLIGHTID";
 
+    /** The Google Map object, used to render the flight data. */
     private GoogleMap map; // Might be null if Google Play services APK is not available.
 
+    /** Flag to ensure that this Activity is started with a flight ID in its intent. */
     private boolean startedWithValidIntent;
+    /** The Flight ID whose path coordinates will be plotted */
     private long flightID = 0;
 
+    //Called when the activity is created. Simply initializes fields from intent.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_detail);
 
+        //Get the intent data to figure out which flight ID we're rendering
         startedWithValidIntent = false;
         Bundle intentExtras = getIntent().getExtras();
         if(intentExtras.containsKey(INTENT_ID_INFO)) {
@@ -33,6 +42,8 @@ public class FlightDetailActivity extends FragmentActivity {
         setUpMapIfNeeded();
     }
 
+    //Called when the activity is resumed;
+    //primarily done to handle Google Play interruptions
     @Override
     protected void onResume() {
         super.onResume();
@@ -52,8 +63,7 @@ public class FlightDetailActivity extends FragmentActivity {
      * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
      * have been completely destroyed during this process (it is likely that it would only be
      * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
+     * method in {@link #onResume()} to guarantee that it will be called. */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (map == null) {
@@ -67,17 +77,17 @@ public class FlightDetailActivity extends FragmentActivity {
         }
     }
 
-    /** (Auto-generated docs.)
-     * This is where we can add markers or lines, add listeners or move the camera.
-     * <p>
-     * This should only be called once and when we are sure that {@link #map} is not null.
-     */
+    /** This method is invoked when the activity starts,
+     * Google Maps is confirmed to be installed,
+     * and the map is ensured to be non-null.
+     * When this is done, the points provided by the ID are plotted using an
+     * {@link edu.erau.mad.trb.flightdatarecorder.AsyncMapDatabasePlotter}. */
     private void setUpMap() {
         if(startedWithValidIntent) {
             new AsyncMapDatabasePlotter(flightID, map, this).execute();
         } else {
-            //TODO handle activity starts without data?
-            Toast.makeText(this, "No flight data was provided!",
+            //TO-DO handle activity starts without data?
+            Toast.makeText(this, "No flight data was present!",
                     Toast.LENGTH_LONG).show();
         }
     }
